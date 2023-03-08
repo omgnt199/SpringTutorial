a96b6a4b4ab92dd0de7d4b658bf7cfb8f0ada3d3
omgnt199 <omgnt199@gmail.com>
2023-03-08 13:46:25 +0700

    USer change

--- demo/src/main/java/com/demo/demo/model/user.java
+++ demo/src/main/java/com/demo/demo/model/user.java
@@ -6,4 +6,26 @@ import jakarta.persistence.Entity;
 public class user {
     private String name;
     private String id;
+
+    public user(){
+
+    }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
 }
